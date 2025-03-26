2bfb9a323eb6bacc80437536dbecc9ac78d4a08b
bikin-brett <bikinbrett64@k-state.edu>
2025-03-26 16:31:29 -0500

    Much functionality added, but writing to the list reg doesn't work.

--- Custom_OpenLog/Custom_OpenLog.cpp
+++ Custom_OpenLog/Custom_OpenLog.cpp
@@ -12,7 +12,7 @@ Custom_OpenLog::Custom_OpenLog(uint8_t addr) {
 
 bool Custom_OpenLog::begin() {
 	Wire.beginTransmission(_address);
-	Wire.write(0x05);
+	Wire.write(INIT_REG);
 	if (Wire.endTransmission() != 0) {
 		return false;
 	}
@@ -23,54 +23,60 @@ void Custom_OpenLog::getStatus(char* status) {
 	//Wire.requestFrom(_address, 1);
 	//&status = Wire.read();
 	
-	(void)readRegister(0x01, status, 1);
+	(void)readRegister(STATUS_REG, status, 1);
 }
 
 void Custom_OpenLog::createFile(char* filename) {
-	
+	writeRegister(NEW_FILE_REG, filename);
 }
 
 void Custom_OpenLog::changeDirectory(char* dir) {
-	
+	writeRegister(CHANGE_DIRECTORY_REG, dir);
 }
 
-char* Custom_OpenLog::readFile(char* filename, char* dest, long num) {
-	return dest;
+char* Custom_OpenLog::readFile(char* filename, char* dest, unsigned long num) {
+	return readRegister(READ_FILE_REG, filename, dest, num);
 }
 
-void Custom_OpenLog::appendFile(char c) {
-	
-}
-
-void Custom_OpenLog::appendFile(char* str) {
-	
-}
-
-void Custom_OpenLog::writeFile(char c) {
-	
+void Custom_OpenLog::openFile(char* filename) {
+	writeRegister(OPEN_FILE_REG, filename);
 }
 
 void Custom_OpenLog::writeFile(char* str) {
-	
+	writeRegister(WRITE_FILE_REG, str);
 }
 
 
 unsigned long Custom_OpenLog::fileSize(char* filename) {
-	return 0;
+	unsigned long result = 0;
+	char output[4];
+	memset(output, 0, 4);
+	(void)readRegister(FILE_SIZE_REG, filename, output, 4);
+	for (int i = 3; i >= 0; i--) {
+		result = result | (output[i] << ((3 - i) * 8));  // Translating four consecutive bytes into a long.
+	}
+	return result;
 }
 
 
 char* Custom_OpenLog::list(char* search, char* dest) {
-	return dest;
+	return readRegister(LIST_REG, search, dest, 10000);
 }
 
 
 void Custom_OpenLog::remove(char* filename) {
-	
+	writeRegister(REMOVE_FILE_REG, filename);
 }
 
 void Custom_OpenLog::removeDirectory(char* dir) {
-	
+	writeRegister(REMOVE_DIRECTORY_REG, dir);
+}
+
+bool Custom_OpenLog::exists(char* filename) {
+	char output[12];
+	memset(output, 0, 12);
+	(void)readRegister(LIST_REG, filename, output, 12);
+	return output[0] > 0;
 }
 
 char* Custom_OpenLog::readRegister(uint8_t reg, char* dataOut, unsigned long num) {
@@ -89,6 +95,8 @@ char* Custom_OpenLog::readRegister(uint8_t reg, char* dataOut, unsigned long num
 
 char* Custom_OpenLog::readRegister(uint8_t reg, char* dataIn, char* dataOut, unsigned long num) {
 	writeRegister(reg, dataIn);
+	
+	delay(15);
 	
 	unsigned long i = 0;
 	Wire.requestFrom(_address, num);
--- Custom_OpenLog/Custom_OpenLog.h
+++ Custom_OpenLog/Custom_OpenLog.h
@@ -5,6 +5,12 @@
 	
 	Created specifically for my Zenith rocket.
 	
+	Note that this library is intended for use with the address pads shorted
+	together so that the I2C address is always 0x29.
+	
+	Note also that the code does not yet respect the Wire library's 32-byte
+	write buffer.
+	
 	Author: Brett Warren
 */
 
@@ -14,16 +20,29 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#define STATUS_REG 0x01
+#define INIT_REG 0x05
+#define NEW_FILE_REG 0x06
+#define NEW_DIRECTORY_REG 0x07
+#define CHANGE_DIRECTORY_REG 0x08
+#define READ_FILE_REG 0x09
+#define OPEN_FILE_REG 0x0B
+#define WRITE_FILE_REG 0x0C
+#define FILE_SIZE_REG 0x0D
+#define LIST_REG 0x0E
+#define REMOVE_FILE_REG 0x0F
+#define REMOVE_DIRECTORY_REG 0x10
+
 class Custom_OpenLog {
 	private:
 		uint8_t _address;
-		uint8_t _writeBuffer[32];
+		char _writeBuffer[32];
 		char* readRegister(uint8_t reg, char* dataOut, unsigned long num);
 		char* readRegister(uint8_t reg, char* dataIn, char* dataOut, unsigned long num);
 		void writeRegister(uint8_t reg, char* dataIn);
 		
 	public:
-		Custom_OpenLog(uint8_t addr = 0x2A);
+		Custom_OpenLog(uint8_t addr = 0x29);
 		bool begin();
 		
 		void getStatus(char* status);
@@ -31,12 +50,10 @@ class Custom_OpenLog {
 		void createFile(char* filename);
 		void makeDirectory(char* name);
 		void changeDirectory(char* dir);
-		char* readFile(char* filename, char* dest, long num);
+		char* readFile(char* filename, char* dest, unsigned long num);
 		
-		void appendFile(char c);
-		void appendFile(char* str);
+		void openFile(char* filename);
 		
-		void writeFile(char c);
 		void writeFile(char* str);
 		
 		unsigned long fileSize(char* filename);
@@ -45,6 +62,8 @@ class Custom_OpenLog {
 		
 		void remove(char* filename);
 		void removeDirectory(char* dir);
+		
+		bool exists(char* filename);
 };
 
 #endif
